ed84c8fe6f57e5ce0f1628a63bde7455525f7ecb
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-17 17:35:58 -0700

    Added CoreData

---
+++ CookBookAfterHour/View Controllers/AddRecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddRecipeViewController.swift
+//  CookBookAfterHour
+//
+//  Created by Taylor Lyles on 9/17/19.
+//  Copyright © 2019 Taylor Lyles. All rights reserved.
+//
+
+import UIKit
+
+class AddRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
